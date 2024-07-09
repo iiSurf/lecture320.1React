6c8e2087214b3b54a63bd81f5234899acd685728
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-09 10:07:03 -0700

    added state and ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { useRef } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ref = useRef(13)
+  const [state, setState] = useState(0);
+
+  function handleClick() {
+    ref.current = ref.current + 1;
+  }
+  function setNewState() {
+    setState(ref.current);
+    // setState(state => state.ref)
+  }
+
+
+  console.log(ref);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <button onClick={handleClick}>Click Me</button>
+  <button onClick={setNewState}>Set New State</button>
+  <h1>Your state is: {state}</h1>
+  <h1>Your Ref is: {ref.current}</h1>
     </>
   )
 }
